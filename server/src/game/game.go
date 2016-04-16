1a7c9936781152d1f0ddfff4e515f70f99d3a03e
Thierry Berger <tom.kahl@hotmail.fr>
2016-04-16 20:12:09 +0200

    player is moving from unity input !

--- server/src/game/game.go
+++ server/src/game/game.go
@@ -7,32 +7,52 @@ import (
 	"time"
 )
 
-var players []map[string]interface{}
+type Vector2 struct {
+	X float32
+	Y float32
+}
 
-const leftAction string = "left"
-const upAction string = "up"
-const rightAction string = "right"
-const downAction string = "down"
+type Player struct {
+	Id int
+    Name string
+    Position Vector2
+	Speed Vector2
+}
+
+//var players []map[string]interface{}
+var players []Player
+
+type MoveMessage struct {
+    XSpeed    float32
+    YSpeed    float32
+}
 
 func PlayerMessage(id int, body []byte) {
-	var player map[string]interface{}
+	// TODO: thread safe (read)
+	gameId := -1
 	for i := range(players) {
-		if (players[i]["id"] == id) {
-			player = players[i]
+		if (players[i].Id == id) {
+			gameId = i
 		}
 	}
-	if player == nil {
+	if gameId == -1 {
 		fmt.Println("should not happen unknown player: ", id)
 		return
 	}
 	
-	var message interface{}
+	var message MoveMessage
 	err := json.Unmarshal(body, &message)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("player: ", id, " ; says: ", message)
+	fmt.Println("player: ", gameId, " ; says: ", message)
+	//if (message["action"] == "move") {
+		// TODO: thread safe (write) // care previous lock
+		players[gameId].Speed.X = message.XSpeed
+		players[gameId].Speed.Y = message.YSpeed
+	//}
+	
 }
 
 func NewPlayer(id int) {
@@ -42,24 +62,23 @@ func NewPlayer(id int) {
         // Slice is full; must grow.
         // We double its size and add 1, so if the size is zero we still grow.
 		
-		newSlice := make([]map[string]interface{}, n, 2*n+1)
+		newSlice := make([]Player, n, 2*n+1)
         copy(newSlice, players)
         players = newSlice
     }
     players = players[0 : n+1]
-    players[n] = map[string]interface{}{
-		"id": id,
-		"name": "Vrixyz",
-		"position": map[string]interface{}{
-			"x": 1,
-			"y": 2},
+    players[n] = Player{
+		id,
+		"Vrixyz",
+		Vector2{1, 2},
+		Vector2{0, 0},
     }
 }
 
 func LostPlayer(id int) {
 	// TODO: thread safe (write)
 	for i := range players {
-		if (players[i]["id"] == id) {
+		if (players[i].Id == id) {
 			n := len(players) - 1
 			players[i] = players[n]
 			players = players[:n]
@@ -69,22 +88,37 @@ func LostPlayer(id int) {
 
 func Start() {
 	
-    i := 0
-    for {
-        i++
-		// TODO: thread safe (read)
-		data := map[string]interface{}{
-				"players": players,
-				"time": time.Now(),
+	// goroutine listens for players
+    go func () {
+		for {
+			// TODO: thread safe (read)
+			data := map[string]interface{}{
+					"players": players,
+					"time": time.Now(),
+				}
+			buf, err := json.Marshal(data)
+			if (err != nil) {
+				fmt.Println("Couldn't marshall player", err)
+			} else {
+				fmt.Println(data)
+				//fmt.Println(buf)
+				udpServer.Broadcast(buf)
 			}
-        buf, err := json.Marshal(data)
-		if (err != nil) {
-			fmt.Println("Couldn't marshall player", err)
-		} else {
-			fmt.Println(data)
-			//fmt.Println(buf)
-			udpServer.Broadcast(buf)
+			time.Sleep(time.Millisecond * 200)
 		}
-        time.Sleep(time.Second * 1)
-    }
+	}	()
+	
+	lastTick := time.Now()
+	for {
+		elapsedTime := time.Now().Sub(lastTick)
+		lastTick = time.Now()
+		// TODO: thread safe
+		for i := range players {
+			// TODO: optimize this shit
+			fmt.Println(players[i])
+			players[i].Position.X = players[i].Position.X + (players[i].Speed.X * float32(elapsedTime.Seconds()))
+			players[i].Position.Y = players[i].Position.Y + (players[i].Speed.Y * float32(elapsedTime.Seconds()))
+		}
+		time.Sleep(time.Millisecond * 200)
+	}
 }
