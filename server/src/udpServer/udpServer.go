09e1822ea77af915d8c4809d487d4a2d2095ebdd
Thierry Berger <tom.kahl@hotmail.fr>
2016-04-17 12:04:55 +0200

    more logs

--- server/src/udpServer/udpServer.go
+++ server/src/udpServer/udpServer.go
@@ -121,7 +121,7 @@ func Start() {
 	address, err := ExternalIP()
 	CheckError(err)
 	MyAddress = address + ":0"
-	fmt.Println(MyAddress)
+	fmt.Println( "my address: ", MyAddress)
     /* Lets prepare a address at any address at port 10003*/   
     ServerAddr,err := net.ResolveUDPAddr("udp",":10003")
     CheckError(err)
